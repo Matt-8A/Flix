2baffadeddad609f6ee275e54837df69553d9b3e
Matt <moimportant.important@gmail.com>
2022-03-04 02:00:53 -0800

    push part 2 of Flix Assignment

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Matthew Ochoa on 3/4/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
